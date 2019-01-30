c7464793e1bfbeb6012a33a388009d400d3234c7
IvyAutumn <gaoqian9588@126.com>
2019-01-30 11:30:37 +0800

    goodsList using axios

--- ImoocMall/src/views/GoodsList.vue
+++ ImoocMall/src/views/GoodsList.vue
@@ -28,13 +28,13 @@
           <div class="accessory-list-wrap">
             <div class="accessory-list col-4">
               <ul>
-                <li>
+                <li v-for="item of goodsList" :key="item.productId">
                   <div class="pic">
-                    <a href="#"><img src="/static/1.jpg" alt=""></a>
+                    <a href="#"><img v-bind:src="'/static/'+item.prodcutImg" alt=""></a>
                   </div>
                   <div class="main">
-                    <div class="name">XX</div>
-                    <div class="price">XX</div>
+                    <div class="name">{{item.productName}}</div>
+                    <div class="price">{{item.prodcutPrice}}</div>
                     <div class="btn-area">
                       <a href="javascript:;" class="btn btn--m">加入购物车</a>
                     </div>
@@ -58,17 +58,29 @@
   import NavHeader from '@/components/NavHeader.vue'
   import NavFooter from '@/components/NavFooter.vue'
   import NavBread from '@/components/NavBread.vue'
+  import axios from 'axios'
 export default {
   name: 'GoodsList',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      goodsList: []
     }
   },
   components: {
     NavHeader,
     NavFooter,
     NavBread
+  },
+  mounted: function () {
+    this.getGoodsList()
+  },
+  methods:{
+    getGoodsList() {
+      axios.get("/goods/list").then((result)=>{
+        var res = result.data;
+        this.goodsList = res.result;
+      })
+    }
   }
 }
 </script>
